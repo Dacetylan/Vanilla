68062f62bafdf408c3a357a05ab1a9a6a5b7bb69
Birk Tamm <45733732+Dacetylan@users.noreply.github.com>
2023-09-24 11:58:09 +0000

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,5 @@
+git remote set-url origin https://Dacetylan@github.com/Dacetylan/Vanilla
+cd ~/Vanilla/data
+git add .
+git commit -m "Automated commit from Vanilla temperature and humidity tracker"
+git push
