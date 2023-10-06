6d201bc3811894d34948a44e79490384ceb1feee
Birk Joonas Tamm <dacetylan@tutanota.com>
2023-10-06 12:10:19 +0100

    Automated commit from Vanilla temperature and humidity tracker

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ sudo pigpiod
 while [ "$local" -lt "$zanzibar" ]
 do
 	zanzibar=$(python3 zanzibar.py)
-	local=$(python3 DHT.py 27)
+	local=$(python3 DHT.py 27 | tr -cd '0-9' | bc)
 	python3 mechanics.py
 done
 
